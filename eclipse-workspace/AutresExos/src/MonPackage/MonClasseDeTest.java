87bf352dbee2e429fdbad3fcd73e05a9cda5a4d2
sthiry2 <sthiry@arfp.asso.fr>
2021-12-01 11:01:01 +0100

    ST AJout Exo Moyenne
    Ajout de l'exo moyenne en java avec le pseudo code en commentaire

---
+++ eclipse-workspace/AutresExos/src/MonPackage/MonClasseDeTest.java
@@ -0,0 +1,10 @@
+package MonPackage;
+
+public class MonClasseDeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("plus simple!");
+	}
+
+}
